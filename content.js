96c95a9869fbf916ab35a1fd1dd55e803cebb10d
Ronit Patel <ronitpatel884@gmail.com>
2024-06-13 23:57:37 -0500

    html displaying

--- content.js
+++ content.js
@@ -1,7 +1,34 @@
-window.addEventListener('load', () => {
-
-    document.querySelector('.T-I.T-I-KE.L3').innerHTML = "HII";
-    //add button
+function addHTMLToGmail() {
+    // Create a new div element with your HTML content
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+    <div style="background-color: yellow; padding: 10px;">
+      Your custom HTML content goes here.
+      <button id="customButton">Click Me!</button>
+    </div>
+      <script>
+        document.getElementById('customButton').addEventListener('click', function() {
+          alert('Button was clicked!');
+        });
+      </script>
+  `;
     
+    const add = document.createElement('button');
+    add.innerHTML = '<button id="btn">Click Me<button>'
 
-});
+    // Find an appropriate place to insert your new div
+    const mainContent = document.querySelector('div[role="main"]');
+    if (mainContent) {
+        mainContent.insertBefore(newDiv, mainContent.firstChild);
+        mainContent.insertBefore(add, mainContent.firstChild);
+
+    }
+  }
+  
+  function func() {
+    alert("Hello! I am an alert box!");
+  }
+  
+  // Execute the function when the page loads
+  window.addEventListener('load', addHTMLToGmail);
+  
